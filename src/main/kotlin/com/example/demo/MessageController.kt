994f8ebf1054e720b01dc2b707ee19e3ccdda8fa
nita-san <ysnitta@gmail.com>
2024-08-22 23:18:25 +0900

    use data class

--- src/main/kotlin/com/example/demo/MessageController.kt
+++ src/main/kotlin/com/example/demo/MessageController.kt
@@ -1,11 +1,15 @@
 package com.example.demo
 
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class MessageController {
     @GetMapping
-    fun index(@RequestParam("name") name:String) = "Hello $name!"
+    fun index() = listOf(
+        Message("1", "Hello!"),
+        Message("2", "Bonjour!"),
+        Message("3", "Privet!")
+    )
+//    fun index(@RequestParam("name") name:String) = "Hello $name!"
 }

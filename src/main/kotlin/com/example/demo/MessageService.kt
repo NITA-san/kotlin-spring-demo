4b1dbbd6d6448f5c75266058ab39fe92e64789b2
nita-san <ysnitta@gmail.com>
2024-08-24 15:08:41 +0900

    extend get message by id

--- src/main/kotlin/com/example/demo/MessageService.kt
+++ src/main/kotlin/com/example/demo/MessageService.kt
@@ -1,6 +1,7 @@
 package com.example.demo
 
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.query
 import org.springframework.stereotype.Service
 import java.util.UUID
 
@@ -12,6 +13,10 @@ class MessageService(val db: JdbcTemplate) {
 
     fun save(message: Message) {
         val id = message.id ?: UUID.randomUUID().toString()
-        db.update("insert into message values (?, ?)", id, message.text);
+        db.update("insert into messages values (?, ?)", id, message.text);
     }
+
+    fun findMessageById(id: String): List<Message> = db.query("select * from messages where id = ?", id)
+    { response, _ -> Message(response.getString("id"), response.getString("text")) }
+
 }
